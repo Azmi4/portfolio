901c0cf4e5ca92a53a0b3383311c26e42f43e9b3
Azamat Arslangaraev <arsazi1994@gmail.com>
2019-10-23 17:37:15 +0500

    ДЗ2 на проверку

--- src/scripts/works.js
+++ src/scripts/works.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 
 const thumbs = {
   template: "#slider-thumbs",
-  props: ["works"]
+  props: ["works", "currentWork"]
 };
 
 const btns = {
@@ -12,17 +12,23 @@ const btns = {
 const display = {
   template: "#slider-display",
   components: { thumbs, btns },
-  props: ["works", "currentWork"]
+  props: ["works", "currentWork", "currentIndex"]
 };
 
 const tags = {
-  template: "#slider-tags"
+  template: "#slider-tags",
+  props: ["tags"]
 };
 
 const info = {
   template: "#slider-info",
   components: { tags },
-  props: ["currentWork"]
+  props: ["currentWork"],
+  computed: {
+    tagsArray() {
+      return this.currentWork.skills.split(',')
+    }
+  }
 };
 
 new Vue({
@@ -31,21 +37,44 @@ new Vue({
   components: { display, info },
   data: () => ({
     works: [],
-    currentWork: {}
+    currentIndex: 0
   }),
+  computed: {
+    currentWork() {
+      return this.works[this.currentIndex];
+    }
+  },
+  watch: {
+    currentIndex(value) {
+      this.makeInfiniteLoop(value);  
+    }
+  },
   methods: {
+    makeInfiniteLoop(value) {
+      const worksAmount = this.works.length -1;
+      if (value < 0) this.currentIndex = worksAmount;
+      if (value > worksAmount) this.currentIndex = 0;
+    },
     makeArrWithRequiredImages(data) {
       return data.map(item => {
-        const requiredPic = require(`../images/content/bg/${item.photo}`);
+        const requiredPic = require(`../images/content/${item.photo}`);
         item.photo = requiredPic;
         return item;
       });
+    },
+    handleSlide(direction) {
+      switch (direction) {
+        case "next":
+          this.currentIndex++;
+          break;
+        case "prev":
+          this.currentIndex--;
+          break;
+      }
     }
   },
   created() {
     const data = require("../data/works.json");
     this.works = this.makeArrWithRequiredImages(data);
-
-    this.currentWork = this.work[0];
   }
 });
